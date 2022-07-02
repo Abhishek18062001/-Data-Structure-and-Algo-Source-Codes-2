27889e0daed37dcd93b85ecb0dfa3da5b9684422
Abhishek Dixit <56883498+Abhishek18062001@users.noreply.github.com>
2022-07-02 08:58:27 +0530

    Create 124.)Implement strstr.cpp

---
+++ 124.)Implement strstr.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int strstr(string ,string);
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string a;
+        string b;
+        
+        cin>>a;
+        cin>>b;
+        
+        cout<<strstr(a,b)<<endl;
+    }
+}
+// } Driver Code Ends
+
+
+//Function to locate the occurrence of the string x in the string s.
+int strstr(string s, string x)
+{
+     //Your code here
+      int n=s.length();
+   int m=x.length();
+   for(int i=0;i<n;i++){
+       string y=s.substr(i,m);
+       if(x==y){
+           return i;
+       }
+   }
+   return -1;
+}
