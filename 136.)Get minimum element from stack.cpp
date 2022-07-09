f52ec522971f5bbe68880e08a0d2b9d7d371e518
Abhishek Dixit <56883498+Abhishek18062001@users.noreply.github.com>
2022-07-09 08:05:38 +0530

    Create 136.)Get minimum element from stack.cpp

---
+++ 136.)Get minimum element from stack.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+/*
+The structure of the class is as follows
+class _stack{
+stack<int> s;
+int minEle;
+public :
+    int getMin();
+    int pop();
+    void push(int);
+};
+*/
+
+class Solution{
+    
+    stack<pair<int,int>> s;
+    public:
+    
+       /*returns min element from stack*/
+       int getMin(){
+           if(s.empty())return -1;
+           return s.top().second;
+           //Write your code here
+       }
+       
+       /*returns poped element from stack*/
+       int pop(){
+           if(s.size()==0)return -1;
+           int x=s.top().first;
+           s.pop();
+           
+           return x;
+           //Write your code here
+       }
+       
+       /*push element x into the stack*/
+       void push(int x){
+           if(s.size()==0){
+               s.push({x,x});
+               
+           }else{
+               int y=min(s.top().second,x);
+               s.push({x,y});
+           }
+           
+           //Write your code here
+       }
+};
+
+// { Driver Code Starts.
+
+int main()
+ {
+    long long t;
+
+    cin>>t;
+    while(t--)
+    {
+        int q;
+        cin>>q;
+        Solution ob;
+        while(q--){
+           int qt;
+           cin>>qt;
+           if(qt==1)
+           {
+              //push
+              int att;
+              cin>>att;
+              ob.push(att);
+           }
+           else if(qt==2)
+           {
+              //pop
+              cout<<ob.pop()<<" ";
+           }
+           else if(qt==3)
+           {
+              //getMin
+              cout<<ob.getMin()<<" ";
+           }
+       }
+       cout<<endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
